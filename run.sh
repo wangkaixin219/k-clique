f8d29d9ff5f9b5b6082c7d9efbabf68fc194ba06
wangkaixin219 <bhsf151004@126.com>
2022-04-08 10:00:10 +0000

    small modification

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 make
 
 ./k-clique -g 1000 0.4
-echo "\n"
+echo
 
 for k in {3..10}
 do 
-    echo "k = $k\n"
+    echo "k = $k"
     ./k-clique -r syn.edges -k $k -o degeneracy
     ./k-clique -r syn.edges -k $k -o degree
     ./k-clique -r syn.edges -k $k -o random
